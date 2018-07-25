5f54eaaa7506f177a7798a53c255502c2315fa2c
Lym <41351569+Nucliam@users.noreply.github.com>
2018-07-25 12:45:24 +1000

    Update Form1.vb

--- Form1.vb
+++ Form1.vb
@@ -1,24 +1,26 @@
-Public Class frmRPGProject
+Public Class frmRPGproject
 
     '****Defines variables used universally****
-    Dim exp, lvl, expToLvl, cash As Integer
+    Dim exp, lvl, expToLvl, taskFreq As Integer
+    'TODO Dim cash as Integer
     Dim task, diff As String
-    Dim db As Database = New Database()
-    Dim taskFreq() As Integer = {0, 0, 0, 0}
+    'TODO Dim db As Database = New Database()
+    Dim taskFreqArray() As Integer = {0, 0, 0, 0} 'TODO - 3,4? Skip variable set if possible
 
     Private Sub frmRPGProject_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         '****Sets variables to default or null****
         lvl = 1
         exp = 0
         expToLvl = 5
-        cash = 0
+        'TODO cash = 0
         task = ""
         diff = ""
-
-        taskFreq(0) = 0
-        taskFreq(1) = 0
-        taskFreq(2) = 0
-        taskFreq(3) = 0
+        taskFreq = 0
+        'TODO Skip the below bit earlier if possible
+        taskFreqArray(0) = 0
+        taskFreqArray(1) = 0
+        taskFreqArray(2) = 0
+        taskFreqArray(3) = 0
     End Sub
 
 
@@ -42,25 +44,55 @@ Public Class frmRPGProject
             diff = cboDiff.Text
             If task = "Homework" Or task = "Exercise" Then
                 exp += 3
+                If task = "Homework" Then
+                    taskFreq = 0
+                Else
+                    taskFreq = 1
+                End If
             ElseIf task = "Study" Then
                 exp += 4
+                taskFreq = 2
             Else
                 exp += 5
+                taskFreq = 3
             End If
             If diff = "Difficult" Then
                 exp += 1
             ElseIf diff = "Easy" Then
                 exp -= 1
             End If
+            '****Checks task frequency And deducts EXP if applicable****
+            For i = 0 To 3
+                If taskFreq = i Then
+                    taskFreqArray(i) += 1
+                    For n = -3 To -1
+                        taskFreqArray(i + n) = 0
+                    Next
+                    For n = 1 To 3
+                        taskFreqArray(i + n) = 0
+                    Next
+                    'TODO****ALTERNATIVE TEST****
+                    'For n = NOT i
+                    '   taskFreqArray(n) = 0
+                    'Next
+                End If
+            Next
+            For i = 0 To 3 'TODO <- Needed?
+                If taskFreqArray(i) >= 4 Then
+                    exp -= 1
+                End If
+            Next
             '****Converts EXP to level if applicable****
             If exp >= expToLvl Then
                 exp -= expToLvl
                 lvl += 1
                 expToLvl += 5
+                'TODO cash += x
             End If
             '****Displays Output****
-            lblExp.text = exp
-            lblLvl.text = lvl
+            lblExp.Text = ("EXP: " & exp)
+            lblLvl.Text = ("LVL: " & lvl)
+            'TODO lblCash.text = ("$$$: " & cash)
         End If
     End Sub
 
