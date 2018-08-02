44458b8918a443fe5d14762232d050187d237160
Scootzy <41351660+Scootzy@users.noreply.github.com>
2018-08-03 09:48:40 +1000

    Add files via upload

---
+++ frmRPGtitle.vb
@@ -0,0 +1,6 @@
+﻿Public Class frmRPGtitle
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        Me.Hide()
+        ' frmRPGproject.show()
+    End Sub
+End Class
